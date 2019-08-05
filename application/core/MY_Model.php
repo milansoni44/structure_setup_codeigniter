4a1c2f6c027af0e3794f440d69d1774886a55d3f
Milan Soni <milans@ehealth.local>
2019-08-05 18:20:10 +0530

    MY_Model change

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MY_Model extends CI_Model{
+	protected $table;
+	protected $joinClause;
+	protected $joinType;
+	protected $fields;
+	protected $joinedTable;
+
 	public function __construct(){
 		parent::__construct();
 	}
@@ -27,9 +33,39 @@ class MY_Model extends CI_Model{
 
 	public function get( $table, $id = NULL, $whereCol = NULL ){
 		if($id){
-			return $this->db->get_where($table, array($whereCol=>$id))->result_row();
+			return $this->db->get_where($table, array($whereCol=>$id, 'status'=>1))->row_array();
 		}else{
-			return $this->db->get($table)->result_array();
+			return $this->db->get_where($table, array('status'=>1))->result_array();
 		}
+	}
+
+	public function maintenance_select($fields){
+		$this->fields = $fields;
+		return $this;
+	}
+
+	public function maintenance_join($joinedTable, $joinClause, $joinType = NULL){
+		$this->joinedTable[] = $joinedTable;
+		$this->joinClause[] = $joinClause;
+		if($joinType){
+			$this->joinType[] = $joinType;
+		}
+		return $this;
+	}
+
+	public function maintenance_get($table){
+		$this->table = $table;
+		$str = "";
+		if(is_array($this->joinedTable) && !empty($this->joinedTable)){
+			for($i = 0; $i < sizeof($this->joinedTable); $i++){
+				$str .= " ".$this->joinType[$i]." JOIN ".$this->joinedTable[$i]." ON ".$this->joinClause[$i];
+			}
+		}
+
+		return $this->db->query("SELECT 
+					$this->fields
+				FROM $this->table
+				$str
+		")->result_array();
 	}
 }
